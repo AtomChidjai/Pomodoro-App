8d8a1563053d091cd01a6672e53a8c7f17058eb0
AtomChidjai <atomchidjai@gmail.com>
2024-06-07 16:42:50 +0700

    completed functionality

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let intervalID;
 let startchange = false;
 let startstart = false;
 
+let starttime2 = 5;
+let time2 = starttime2 * 60;
+let timer2 = document.getElementById('countdown-timer2');
+let startbtn2 = document.getElementById('start-button2');
+let intervalID2;
+let startchange2 = false;
+let startstart2 = false;
+
 function StartTimer() {
     if(startchange){
         startbtn.innerHTML = 'Start';
@@ -45,4 +53,45 @@ function ResetTimer () {
     startstart = false;
     startbtn.innerHTML = 'Start';
     startchange = false;
+}
+
+function StartTimer2 () {
+    if(startchange2){
+        startbtn2.innerHTML = 'Start';
+    }
+    if(startstart2 == false){
+        intervalID2 = setInterval(CountdownTimer2, 1000);
+        startstart2 = true;
+    }
+}
+
+function CountdownTimer2 () {
+    let minutes = Math.floor(time2 / 60);
+    let seconds = time2 % 60;
+    
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    timer2.innerHTML = `${minutes} : ${seconds}`;
+    time2--;
+
+    if (time2 < 0) {
+        clearInterval(intervalID2);
+        timer2.innerHTML = "00 : 00";
+    }
+}
+
+function PauseTimer2 () {
+    clearInterval(intervalID2);
+    startbtn2.innerHTML = 'Resume';
+    startchange2 = true;
+    startstart2 = false;
+}
+
+function ResetTimer2 () {
+    clearInterval(intervalID);
+    timer2.innerHTML = "5 : 00";
+    time2 = 5 * 60;
+    startstart2 = false;
+    startbtn2.innerHTML = 'Start';
+    startchange2 = false;
 }
